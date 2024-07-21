2fe99766871ca2cfd11be157ded6ad8571af79b9
Samuel Andrade de Araújo <andradedearaujo100@gmail.com>
2024-07-21 09:07:27 -0300

    Configurando ambiente e criando tipos de dados.

---
+++ src/types/tecnologia.ts
@@ -0,0 +1,9 @@
+type Tecnologia = {
+    id: string, 
+    titulo: string,
+    estudado: boolean,
+    data_fim: Date,
+    data_criacaoo: Date
+}
+
+export default Tecnologia
---
+++ src/types/usuario.ts
@@ -0,0 +1,8 @@
+import Tecnologia from './tecnologia'
+
+type Usuario = {
+    id: string,
+    nome: string,
+    usuario_nome: string,
+    tecnologias: Tecnologia[]
+}
