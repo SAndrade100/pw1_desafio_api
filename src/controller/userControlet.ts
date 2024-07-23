80057b3acaa61e1221a2a6b6dee96cb80c0ef8fc
Samuel Andrade de Araújo <andradedearaujo100@gmail.com>
2024-07-23 07:46:02 -0300

    Concluíndo controlador.

--- src/controller/userControlet.ts
+++ src/controller/userControlet.ts
@@ -57,16 +57,63 @@ class UserController {
 
     updateTechnology(req: Request, res: Response) {
         const db = new Database(dados)
-        const { name, userName } : { name: string, userName: string } = req.body
+        const { userName } : { userName: string } = req.body
+        const { title, deadline } : { title: string, deadline: string } = req.body
+        const { id }: { id: string } = req.params as { id : string }
+
+        const idUser = db.findUserByUserName(userName)
+        const technology = db.getTecnologyById(idUser, id)
+        if(!technology){
+            return res.status(404).json({error: "Tecnologia não existe no banco de dados."})
+        }
+
+        if(!!deadline && new Date(deadline) < new Date()){
+            return res.status(400).json({error: "Data inválida."})
+        }
+
+        const newTechnology: Technologies = {
+            ...technology,
+            title: !!title ? title : technology.title,
+            deadline: deadline ? new Date(deadline) : technology.deadline
+        }
+
+        
+        const addTechnology = db.updateTechnology(userName, id, newTechnology)
+        res.status(200).json(addTechnology)
     }
 
     updateTechnologyStatus(req: Request, res: Response) {
         const db = new Database(dados)
-        const { name, userName } : { name: string, userName: string } = req.body
+        const { userName } : { userName: string } = req.body
+        const { id }: { id: string } = req.params as { id: string }
+
+        const idUser = db.findUserByUserName(userName)
+        const technology = db.getTecnologyById(idUser, id)
+
+        if(!technology){
+            return res.status(404).json({error: "Technology not found"})
+        }
+
+        const newTechnology: Technologies = {
+            ...technology,
+            studied: true
+        } 
+
+        const updatedTechnology = db.updateTechnology(userName, id, newTechnology)
+        res.status(201).json(updatedTechnology)
     }
 
     deleteTechnology(req: Request, res: Response) {
         const db = new Database(dados)
-        const { name, userName } : { name: string, userName: string } = req.body
+        const { userName } : { userName: string } = req.headers as { userName: string } 
+        const { id }: { id: string } = req.params as { id : string }
+
+        const idUser = db.findUserByUserName(userName)
+        if(!db.getTecnologyById(idUser, id)){
+            return res.status(404).json({error: "Tecnologia não existe no banco de dados."})
+        }
+
+        db.removeTechnology(userName, id)
+        res.status(201).json({status: "Technology removed"})
     }
 }
